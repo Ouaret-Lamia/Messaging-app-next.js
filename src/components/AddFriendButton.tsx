cccc7267312e3a0a14103e028025b8d74aa29ea4
Ouaret Lamia <ouaretlamia04@gmail.com>
2025-04-23 23:24:15 +0200

    send friend request api

--- src/components/AddFriendButton.tsx
+++ src/components/AddFriendButton.tsx
@@ -58,10 +58,10 @@ const AddFriendButton: FC<AddFriendButtonProps> = ({}) => {
 
       <div className="mt-2 flex gap-4">
         <input
-          {...register("email")}
-          type="text"
-          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-          placeholder="you@example.com"
+          {...register('email')}
+          type='text'
+          className='block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+          placeholder='you@example.com'
         />
         <Button>Add</Button>
       </div>
